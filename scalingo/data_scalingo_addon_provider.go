71c1bd90ddea9d47b13846015e11a61372f31e63
Quentin Escudier <quentin.escudier@scalingo.com>
2022-09-13 16:47:09 +0200

    feat(provider): implement data source addon provider
    scalingo_addon_providers:
    
    require name / return name+id+logo_url
    
    Feat #49

---
+++ scalingo/data_scalingo_addon_provider.go
@@ -0,0 +1,72 @@
+package scalingo
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/Scalingo/go-scalingo/v5"
+)
+
+func dataSourceScAddonProvider() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceScAddonProviderRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"logo_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceScAddonProviderRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, _ := meta.(*scalingo.Client)
+
+	name, ok := d.Get("name").(string)
+	if !ok || name == "" {
+		return diag.Errorf("name attribute is mandatory")
+	}
+
+	addonProviders, err := client.AddonProvidersList(ctx)
+
+	if err != nil {
+		return diag.Errorf("fail to fetch addon providers: %v", err)
+	}
+
+	var selected *scalingo.AddonProvider
+
+	for _, v := range addonProviders {
+		if v.Name == name {
+			selected = v
+			break
+		}
+	}
+
+	if selected == nil {
+		return diag.Errorf("addon provider '%v' not found", name)
+	}
+
+	d.SetId(selected.ID)
+	err = SetAll(d, map[string]interface{}{
+		"id":       selected.ID,
+		"name":     selected.Name,
+		"logo_url": selected.LogoURL,
+	})
+
+	if err != nil {
+		return diag.Errorf("fail to set addon providers: %v", err)
+	}
+
+	return nil
+}
