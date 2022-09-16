6e54575fccd7edb7f7dab371021b958def416706
CURZOLA Pierre <pierre.curzola@gmail.com>
2022-09-16 10:38:23 +0200

    chore(utils): add filter using generic

--- scalingo/utils_slice.go
+++ scalingo/utils_slice.go
@@ -1,5 +1,18 @@
 package scalingo
 
+// keepIf deletes every element of slice for which the given test function return false.
+// It returns the filtered slice.
+func keepIf[S any](slice []S, test func(S) bool) []S {
+	var filteredElem []S
+
+	for _, s := range slice {
+		if test(s) {
+			filteredElem = append(filteredElem, s)
+		}
+	}
+	return filteredElem
+}
+
 func Contains(s []string, v string) bool {
 	for _, c := range s {
 		if c == v {
