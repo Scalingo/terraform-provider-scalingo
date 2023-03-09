e10d6db743bdd5dc35be466c4647f06673c66e49
Joseph Page <josephpage@users.noreply.github.com>
2023-03-09 14:48:26 +0100

    docs(stack): a better example for scalingo_stack data-source

--- examples/data-sources/scalingo_stack/data-source.tf
+++ examples/data-sources/scalingo_stack/data-source.tf
@@ -1,3 +1,9 @@
-data_source "scalingo_stack" "scalingo_20" {
-  name = "scalingo-20
+data "scalingo_stack" "my_chosen_stack" {
+  name = "scalingo-22"
+}
+
+resource "scalingo_app" "my_test_app" {
+  name = "terraform-testapp"
+
+  stack_id = data.scalingo_stack.my_chosen_stack.id
 }
